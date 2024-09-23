3387c1a3c38d05913be253e9d6b41d50a87f410d
PresentKim <debe3721@gmail.com>
2024-09-23 14:12:06 +0900

    fix: 누락된 그래들 의존성 추가 - 웹 요청을 처리하기 위한 `spring-boot-starter-web` - JWT 를 처리하기 위한 `io.jsonwebtoken:jjwt-*`

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,8 @@ dependencies {
     // 개발 환경 관련 의존성
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    // 유효성 검사 관련 의존성
+    // 웹 요청 처리 관련 의존성
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // 데이터 베이스 관련 의존성
@@ -37,11 +38,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-mail")
 
+    // JWT 관련 의존성
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     // 테스트 관련 의존성
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.springframework.security:spring-security-test")
 
     // 기타 의존성
     implementation("org.jetbrains.kotlin:kotlin-reflect")
